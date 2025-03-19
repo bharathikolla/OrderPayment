69148f6516fa0ff9f5b3dab3cfd4c3a969d892b6
Mahesh <maheshkolla14@gmail.com>
2025-03-19 15:25:58 +0530

    Created the model classes

---
+++ SamplePayment/src/main/java/com/example/SamplePayment/model/OrderRequest.java
@@ -0,0 +1,9 @@
+package com.example.SamplePayment.model;
+
+
+public class OrderRequest {
+	
+	 private OrderType orderType;
+	 private String productName;
+
+}
---
+++ SamplePayment/src/main/java/com/example/SamplePayment/model/OrderType.java
@@ -0,0 +1,10 @@
+package com.example.SamplePayment.model;
+
+public enum OrderType {
+	PRODUCT,
+    BOOK,
+    MEMBERSHIP,
+    MEMBERSHIP_UPGRADE,
+    VIDEO
+
+}

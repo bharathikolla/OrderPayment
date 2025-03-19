51b64b67fdd90c71da9ceb5af704a736a96869f1
Mahesh <maheshkolla14@gmail.com>
2025-03-19 15:09:02 +0530

    initial commit

---
+++ SamplePayment/src/main/java/com/example/SamplePayment/SamplePaymentApplication.java
@@ -0,0 +1,13 @@
+package com.example.SamplePayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplePaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplePaymentApplication.class, args);
+	}
+
+}
---
+++ SamplePayment/src/test/java/com/example/SamplePayment/SamplePaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SamplePayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplePaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
